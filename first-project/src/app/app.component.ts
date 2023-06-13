1b87e13e683d9741beb0fe9673a95e92defec791
Leonardo <lleonardo1555@gmail.com>
2023-06-13 00:47:56 -0300

    Ciclo de vida: ngOnChages()

--- first-project/src/app/app.component.ts
+++ first-project/src/app/app.component.ts
@@ -2,15 +2,23 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<router-outlet></router-outlet>'
+  template: `
+    <app-title title="Olá Mundo"></app-title>
+    <router-outlet></router-outlet>
+  `
 })
 export class AppComponent implements OnInit {
   constructor() {}
-
+  
+  // ngOnInit Este evento é inicializado após o Angular exibir pela primeira vez as propriedades vinculadas aos dados
+  // ou quando o componente foi inicializado. Este evento é usado principalmente para inicializar dados em um componente
+  
   ngOnInit() : void {
     setTimeout(() => {
       console.log(1);
     }, 5000);
   }
+
+  
 
 }
--- first-project/src/app/title/title.component.ts
+++ first-project/src/app/title/title.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.scss']
 })
-export class TitleComponent {
+export class TitleComponent implements OnInit, OnChanges {
 
-  public title: string = "Bem vindo!";
+  @Input() public title: string = "Bem vindo!";
+
+  ngOnInit(): void {
+    
+  }
+  
+  // Este evento é executado sempre que um valor de um controle de entrada dentro do componente é alterado.
+  // Sempre que um componente recebe um dado através @Input() o ngOnChanges() é invocado
+  ngOnChanges(): void {
+    console.log("Foi alterado com sucesso!")
+  }
 }
