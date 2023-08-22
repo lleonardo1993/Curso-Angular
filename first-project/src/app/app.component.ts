83ea35eb41bbff86689c9dea045b9c71f67a6719
Leonardo <lleonardo1555@gmail.com>
2023-08-21 21:24:30 -0300

    Ciclos de vida do Angular;

--- first-project/src/app/app.component.ts
+++ first-project/src/app/app.component.ts
@@ -1,13 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit,  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewChecked,
+  AfterViewInit } from '@angular/core';
+/*
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewChecked,
+  AfterViewInit
+*/
 @Component({
   selector: 'app-root',
   template: `
-    <app-title title="Olá Mundo"></app-title>
-    <router-outlet></router-outlet>
+   
+   <app-title title="Olá Mundo"></app-title>
+   {{valor}}
+   <button (click)="adicionar()">Adicionar</button>
+   <router-outlet></router-outlet>
   `
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit,  DoCheck,
+AfterContentInit,
+AfterContentChecked,
+AfterViewChecked,
+AfterViewInit {
+  
+  // variavel tipada com valor / number
+  public valor: number = 1;
+
+  // funcao tipada com tipo de retorno number
+  public adicionar() : number{
+    return this.valor += 1;
+  }
   constructor() {}
   
   // ngOnInit Este evento é inicializado após o Angular exibir pela primeira vez as propriedades vinculadas aos dados
@@ -18,7 +43,25 @@ export class AppComponent implements OnInit {
       console.log(1);
     }, 5000);
   }
+  // ngDoCheck: fica verificando se tem alguma alteração
+  ngDoCheck(): void {
+    console.log("ngDoCheck")    
+  }
+  // ngAfterContentInit: método chamado quando renderizado a pagina
+  ngAfterContentInit(): void {
+    console.log("ngAfterContentInit")    
+  }  
+  // ngAfterContentChecked: quando valor alterado invoca o método 
+  ngAfterContentChecked(): void {
+    console.log("ngAfterContentChecked")  
+  }
+  // ngAfterViewInit: sempre que renderizado o valor na view é chamado o método
+  ngAfterViewInit(): void {
+    console.log("ngAfterViewInit")  
+  } 
 
-  
-
+  // ngAfterViewChecked: método chamado quando renderizado a pagina
+  ngAfterViewChecked(): void {
+    console.log("ngAfterViewChecked")  
+  }
 }
